fcf726d9798fdd7975c3f74d31e0ec1ad8d4ba28
Nikitha Mbokotwana <lonambokotwan117@gmail.com>
2021-06-08 16:45:37 +0200

    First Commit

--- src/main/java/za/ca/cput/Entity/StaffDetails/Demographic.java
+++ src/main/java/za/ca/cput/Entity/StaffDetails/Demographic.java
@@ -1,4 +1,70 @@
 package za.ca.cput.Entity.StaffDetails;
+/* Demographic.java
+   Entity for Demographic
+   Author : Nikitha Mbokotwana (218337906)
+   Date : 08 June
+ */
+
+import java.util.Date;
+import java.util.Locale;
 
 public class Demographic {
+    private String gender;
+    private Date birthDate;
+
+
+    private Demographic(Builder builder) {
+        this.gender= builder.gender;
+        this.birthDate= builder.birthDate;
+    }
+    @Override
+    public String toString() {
+        return "Demographic{" +
+                "gender='" + gender + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    public static class Builder{
+        private String gender;
+        private Date birthDate;
+
+        public Builder setGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder setBirthDate(Date birthDate) {
+            this.birthDate = birthDate;
+            return this;
+        }
+
+        public Builder copy(Demographic demographic) {
+            this.gender = demographic.gender;
+            this.birthDate = demographic.birthDate;
+
+            return this;
+        }
+
+    public Demographic build()
+    {
+        return new Demographic(this);
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/za/ca/cput/Entity/StaffDetails/EmployeeAddress.java
+++ src/main/java/za/ca/cput/Entity/StaffDetails/EmployeeAddress.java
@@ -1,4 +1,21 @@
 package za.ca.cput.Entity.StaffDetails;
 
 public class EmployeeAddress {
+
+    public static class Builder{
+        private String employeeAddress, addressID;
+
+        public Builder setEmployeeAddress(String employeeAddress) {
+            this.employeeAddress = employeeAddress;
+
+            return this;
+        }
+
+        public Builder setAddressID(String addressID) {
+            this.addressID = addressID;
+
+            return this;
+        }
+    }
+
 }
