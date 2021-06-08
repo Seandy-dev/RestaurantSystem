89bf5e7f8cc7c38bcb93226df98c1f325b05d9b0
Hlombekazi <hlombekazimbelu@gmail.com>
2021-06-08 15:56:43 +0200

    Second commit : created a builder class inside the Address class ,added attributes and tostring method.

--- src/main/java/za/ca/cput/Entity/StaffDetails/Address.java
+++ src/main/java/za/ca/cput/Entity/StaffDetails/Address.java
@@ -2,4 +2,38 @@ package za.ca.cput.Entity.StaffDetails;
 
 public class Address {
 
+      private String addressId, streetName ,
+              suburb , city , country;
+      private int streetNumber , postalCode;
+
+
+    public Address(Builder builder)
+      {
+          this.addressId = builder.addressId;
+          this.city = builder.city;
+          this.country = builder.country;
+          this.streetName = builder.streetName;
+          this.suburb = builder.suburb;
+          this.streetNumber = builder.streetNumber;
+          this.postalCode = builder.postalCode;
+
+      }
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressId='" + addressId + '\'' +
+                ", streetName='" + streetName + '\'' +
+                ", suburb='" + suburb + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", streetNumber=" + streetNumber +
+                ", postalCode=" + postalCode +
+                '}';
+    }
+      private static class Builder{
+
+          private String addressId, streetName ,
+                  suburb , city , country;
+          private int streetNumber , postalCode;
+      }
 }
