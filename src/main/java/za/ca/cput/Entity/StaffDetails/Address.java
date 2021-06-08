794f8dc6c40f18714a8cf1411c8c75ff72a2e0f9
Hlombekazi <hlombekazimbelu@gmail.com>
2021-06-08 15:58:50 +0200

    Third commit : added setters in the build class

--- src/main/java/za/ca/cput/Entity/StaffDetails/Address.java
+++ src/main/java/za/ca/cput/Entity/StaffDetails/Address.java
@@ -35,5 +35,33 @@ public class Address {
           private String addressId, streetName ,
                   suburb , city , country;
           private int streetNumber , postalCode;
+
+          public void setAddressId(String addressId) {
+              this.addressId = addressId;
+          }
+
+          public void setStreetName(String streetName) {
+              this.streetName = streetName;
+          }
+
+          public void setSuburb(String suburb) {
+              this.suburb = suburb;
+          }
+
+          public void setCity(String city) {
+              this.city = city;
+          }
+
+          public void setCountry(String country) {
+              this.country = country;
+          }
+
+          public void setStreetNumber(int streetNumber) {
+              this.streetNumber = streetNumber;
+          }
+
+          public void setPostalCode(int postalCode) {
+              this.postalCode = postalCode;
+          }
       }
 }
