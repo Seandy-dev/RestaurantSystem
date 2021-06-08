69f53947e90a493fb14744fde5b807253fb02c71
Sindiswa Mbhele <219279616@mycput.com>
2021-06-08 22:32:05 +0200

    Sindiswa's task

--- src/main/java/za/ca/cput/Entity/Order/Table.java
+++ src/main/java/za/ca/cput/Entity/Order/Table.java
@@ -1,4 +1,58 @@
 package za.ca.cput.Entity.Order;
 
+/* Table.java
+ Entity for Table
+ Author: Sindiswa Nomusa Mbhele (219279616)
+ Date: 07 June 2021
+*/
 public class Table {
+
+
+    private int tableNumber;
+    private boolean occupied;
+
+    public int getTableNumber(){return tableNumber; }
+
+    public boolean getOccupied(){
+        return occupied;
+    }
+
+    public Table(Builder builder){
+        this.tableNumber = builder.tableNumber;
+        this.occupied = builder.occupied;
+    }
+
+    @Override
+    public String toString() {
+        return "Table{" +
+                "tableNumber=" + tableNumber +
+                ", occupied=" + occupied +
+                '}';
+    }
+
+    public static class Builder {
+        private int tableNumber;
+        private boolean occupied;
+
+        public Builder setTableNumber(int tableNumber) {
+            this.tableNumber = tableNumber;
+            return this;
+        }
+
+        public Builder setOccupied(boolean occupied) {
+            this.occupied = occupied;
+            return this;
+        }
+
+        public Builder copy(Table table) {
+            this.tableNumber = table.tableNumber;
+            this.occupied = table.occupied;
+            return this;
+        }
+
+        public Table build() {
+            return new Table(this);
+        }
+
+    }
 }
--- src/main/java/za/ca/cput/Entity/Staff/Waiter.java
+++ src/main/java/za/ca/cput/Entity/Staff/Waiter.java
@@ -1,4 +1,65 @@
 package za.ca.cput.Entity.Staff;
 
+/* Waiter.java
+ Entity for the Waiter
+ Author: Sindiswa Nomusa Mbhele (219279616)
+ Date: 07 June 2021
+*/
 public class Waiter {
+
+
+    private double HourlyWage, Tip;
+    private int Hours;
+
+    public double getHourlyWage(){ return HourlyWage;}
+
+    public double getTip(){return Tip;}
+
+    public int getHours(){return Hours;}
+
+    public Waiter(Builder builder){
+        this.HourlyWage = builder.HourlyWage;
+        this.Tip = builder.Tip;
+        this.Hours = builder.Hours;
+    }
+
+    @Override
+    public String toString() {
+        return "Waiter{" +
+                "HourlyWage=" + HourlyWage +
+                ", Tip=" + Tip +
+                ", Hours=" + Hours +
+                '}';
+    }
+
+    public static class Builder{
+        private double HourlyWage, Tip;
+        private int Hours;
+
+        public Builder setHourlyWage(double hourlyWage) {
+            this.HourlyWage = hourlyWage;
+            return this;
+        }
+
+        public Builder setHours(int Hours){
+            this.Hours = Hours;
+            return this;
+        }
+
+        public Builder setTip(double Tip){
+            this.Tip = Tip;
+            return this;
+        }
+
+        public Builder copy( Waiter waiter){
+            this.HourlyWage = waiter.HourlyWage;
+            this.Hours = waiter.Hours;
+            this.Tip = waiter.Tip;
+            return this;
+        }
+
+        public Waiter build(){
+            return new Waiter (this);
+        }
+    }
 }
