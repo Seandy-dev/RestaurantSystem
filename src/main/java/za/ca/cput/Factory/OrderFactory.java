8b55edfc3358612a949a9041f4c0362e69c5d962
Devon May <219168296@mycput.ac.za>
2021-06-10 22:05:58 +0200

    Factory and TDD

--- src/main/java/za/ca/cput/Factory/OrderFactory.java
+++ src/main/java/za/ca/cput/Factory/OrderFactory.java
@@ -1,4 +1,20 @@
 package za.ca.cput.Factory;
+import za.ca.cput.Entity.Order.Order;
 
-public class OrderFactory {
+/* OrderFactory.java
+   Order Factory
+   Author : Devon Sherwyn May (219168296)
+   Date : 11 June 2021
+*/
+
+public class OrderFactory
+{
+    public  static Order createOrder(int orderNumber, long orderDate)
+    {
+        Order order = new Order.Builder()
+                .setOrderNumber(orderNumber)
+                .setOrderDate(orderDate)
+                .build();
+        return order;
+    }
 }
