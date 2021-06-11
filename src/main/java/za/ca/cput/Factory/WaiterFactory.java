48d22936272d96ca5c46dfc639f3db48d193f8ca
Sindiswa Mbhele <219279616@mycput.com>
2021-06-11 20:04:57 +0200

    Table and Waiter Factory

--- src/main/java/za/ca/cput/Factory/WaiterFactory.java
+++ src/main/java/za/ca/cput/Factory/WaiterFactory.java
@@ -1,4 +1,21 @@
 package za.ca.cput.Factory;
 
+/* WaiterFactoryTest.java
+ Entity for WaiterFactoryTest
+ Author: Sindiswa Nomusa Mbhele (219279616)
+ Date: 10 June 2021
+*/
+import za.ca.cput.Entity.Staff.Waiter;
+
 public class WaiterFactory {
+
+    public  static Waiter createWaiter(double hourlyWage, int hours, double tip) {
+
+        Waiter tb = new Waiter.Builder()
+                .setHourlyWage(hourlyWage)
+                .setHours(hours)
+                .setTip(tip)
+                .build();
+        return tb;
+    }
 }
