b9986cf73c90515c7efde3ce8cd256523fec6002
Mbuyi5 <42034591+Mbuyi5@users.noreply.github.com>
2021-06-11 14:36:45 +0200

    Populated the Factory entity with objects added seters for the builder class

--- src/main/java/za/ca/cput/Factory/AddressFactory.java
+++ src/main/java/za/ca/cput/Factory/AddressFactory.java
@@ -1,4 +1,22 @@
 package za.ca.cput.Factory;
 
+import za.ca.cput.Entity.StaffDetails.Address;
+import za.ca.cput.Util.GenericHelper;
+
 public class AddressFactory {
+    public static Address createAddress(String addressId, int streetNumber, String streetName,String suburb,
+                                        String city, String country , int postalCode )
+    {
+        addressId = GenericHelper.genericHelper();
+        Address address = new Address.Builder()
+                .setAddressId(addressId)
+                .setStreetNumber(streetNumber)
+                .setStreetName(streetName)
+                .setSuburb(suburb)
+                .setCity(city)
+                .setCountry(country)
+                .setPostalCode(postalCode);
+
+        return address;
+    }
 }
