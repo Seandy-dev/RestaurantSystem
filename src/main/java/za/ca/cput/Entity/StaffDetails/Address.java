3a2edf599475128f50d0453b9a4e36d9be2929f1
hlombekazi <209024666@mycput.ac.za>
2021-06-11 19:02:16 +0200

    Revert "Added comments"
    This reverts commit 97c6fdec

--- src/main/java/za/ca/cput/Entity/StaffDetails/Address.java
+++ src/main/java/za/ca/cput/Entity/StaffDetails/Address.java
@@ -1,9 +1,3 @@
-/*Address.java
-* Address entity
-*Author : Hlombekazi Mbelu 209024666
-* Date 8 June 2021
- */
-
 package za.ca.cput.Entity.StaffDetails;
 
 public class Address {
@@ -88,11 +82,6 @@ public class Address {
             this.streetNumber = address.streetNumber;
             this.postalCode = address.postalCode;
             return this;
-        }
-
-        public Address build()
-        {
-            return new Address(this);
         }
     }
 
