389c0a9811a8279efbeaa84a0cc92b019563aad0
hlombekazi <209024666@mycput.ac.za>
2021-06-11 18:57:15 +0200

    Revert "Tests class created all test classes"
    This reverts commit 05ddc3c3

--- src/test/java/za/ca/cput/Factory/AddressFactoryTest.java
+++ src/test/java/za/ca/cput/Factory/AddressFactoryTest.java
@@ -6,43 +6,19 @@
 
 package za.ca.cput.Factory;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
 import za.ca.cput.Entity.StaffDetails.Address;
-
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressFactoryTest {
 
-
-    Address address = AddressFactory.createAddress("", 330,"Mkonto",
-            "Khayelitsha" , "Cape Town", "South Africa", 7784);
     @Test
     public void addressTest()
     {
-        Assertions.assertEquals(address , address);
-
-        Assertions.assertSame(address , address);
-
-    }
-
-    @Test
-    @Disabled
-    public void disableTest()
-    {
-          Assertions.assertEquals(address , address);
-    }
-
-    @Test
-    @Timeout(value = 1000 )
-    public void testTimeOut()
-    {
-        Assertions.assertEquals(address, address);
+        Address address = AddressFactory.createAddress("", 330,"Mkonto",
+                "Khayelitsha" , "Cape Town", "South Africa", 7784);
+        System.out.println(address);
     }
 
 }
