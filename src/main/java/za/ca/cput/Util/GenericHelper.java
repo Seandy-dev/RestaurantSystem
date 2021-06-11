1c0f9cdbcb571a245d740dd0eabd5e775b7c77e8
Mbuyi5 <42034591+Mbuyi5@users.noreply.github.com>
2021-06-11 14:51:24 +0200

    Added package Util, with generic helper class in it.

---
+++ src/main/java/za/ca/cput/Util/GenericHelper.java
@@ -0,0 +1,10 @@
+package za.ca.cput.Util;
+
+import java.util.UUID;
+
+public class GenericHelper {
+    public static String genericHelper() {
+
+        return UUID.randomUUID().toString();
+    }
+}
