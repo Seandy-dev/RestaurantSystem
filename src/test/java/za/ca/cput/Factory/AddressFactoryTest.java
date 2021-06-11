0d9340fb55fec9e96c8059f85e78098cd6baa62d
hlombekazi <209024666@mycput.ac.za>
2021-06-11 19:13:29 +0200

    Revert "Added comments"
    This reverts commit 97c6fdec

--- src/test/java/za/ca/cput/Factory/AddressFactoryTest.java
+++ src/test/java/za/ca/cput/Factory/AddressFactoryTest.java
@@ -6,19 +6,39 @@
 
 package za.ca.cput.Factory;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ca.cput.Entity.StaffDetails.Address;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressFactoryTest {
 
+    Address address = AddressFactory.createAddress("", 330,"Mkonto",
+            "Khayelitsha" , "Cape Town", "South Africa", 7784);
     @Test
     public void addressTest()
     {
-        Address address = AddressFactory.createAddress("", 330,"Mkonto",
-                "Khayelitsha" , "Cape Town", "South Africa", 7784);
-        System.out.println(address);
+        Assertions.assertEquals(address , address);
+
+        Assertions.assertSame(address , address);
+
     }
 
+
+    @Test
+    @Disabled
+    public void disableTest()
+    {
+        Assertions.assertEquals(address , address);
+    }
+
+    @Test
+    @Timeout(value = 1000 )
+    public void testTimeOut()
+    {
+        Assertions.assertEquals(address, address);
+    }
 }
